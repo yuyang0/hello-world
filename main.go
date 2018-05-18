f2dfe5c55c8d579df031793faf1a0e32862a9115
YuYang <yyangplus@gmail.com>
2018-05-18 14:40:33 +0800

    0.0.2

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	containerID, _ := os.Hostname()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world! I'm container: %s. version 0.0.1\n", containerID)
+		fmt.Fprintf(w, "Hello, world! I'm container: %s. version 0.0.2\n", containerID)
 	})
 
 	http.ListenAndServe(":8080", nil)
